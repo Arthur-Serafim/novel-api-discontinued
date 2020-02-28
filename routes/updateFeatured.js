70eb3759d9a95ad645f719b5a7b144e27f017564
Arthur Serafim <arthur@dev.net.br>
2020-02-28 18:05:24 -0300

    Add rejection error

--- routes/updateFeatured.js
+++ routes/updateFeatured.js
@@ -5,8 +5,12 @@ const updateFeatured = require("../controllers/updateFeaturedController");
 /* GET novels listing. */
 // @Route /list
 router.put("/", async (req, res, next) => {
-  const response = await updateFeatured();
-  res.json(response);
+  try {
+    const response = await updateFeatured();
+    res.json(response);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 module.exports = router;
